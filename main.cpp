d4e11245ca08a220175548f19474c4f0c208747e
Thomas Chamelot <chamelot.thomas@gmail.com>
2018-06-15 14:38:17 +0200

    Update main.cpp
    28ème 15/06/2018 14h38

--- main.cpp
+++ main.cpp
@@ -8,21 +8,113 @@
 
 using namespace std;
 
-class Flag
+enum Team
+{
+    orange = 1,
+    purple = -1
+};
+
+class Position
+{
+public:
+  Position(int x = 0, int y = 0, int vx = 0, int vy = 0)
+  {
+    m_x = x;
+    m_y = y;
+    m_vx = vx;
+    m_vy = vy;
+  }
+
+  ~Position()
+  {
+
+  }
+
+  void set_pos(int x, int y, int vx = 0, int vy = 0)
+  {
+    m_x = x;
+    m_y = y;
+    m_vx = vx;
+    m_vy = vy;
+  }
+
+  void set_x(int x)
+  {
+    m_x = x;
+  }
+
+  void set_y(int y)
+  {
+    m_y = y;
+  }
+
+  int get_x()
+  {
+      return m_x;
+  }
+
+  int get_y()
+  {
+      return m_y;
+  }
+
+  int get_vx()
+  {
+    return m_vx;
+  }
+
+  int get_vy()
+  {
+    return m_vy;
+  }
+
+  // Return the estimate position in N steps
+  Position get_n_step_pos(int n)
+  {
+    Position pos;
+    //cerr << "get n step pos res :" << m_x + n*m_vx << " " << m_y + n*m_vy << endl;
+    pos.set_pos(m_x + n*m_vx, m_y + n*m_vy, m_vx, m_vy);
+    //cerr << "get n step pos res :" << pos.m_x << " " << pos.m_y << endl;
+    return pos;
+  }
+
+  // Return the distance between two point in N steps
+  float n_step_distance_from(int n, Position pos)
+  {
+    return sqrt(pow(m_x + n*m_vx - n*pos.get_vx() - pos.get_x(), 2) + pow(m_y + n*m_vy - n*pos.get_vy() - pos.get_y(), 2));
+  }
+
+  // Return the closest Position in N steps
+  Position get_n_step_closest(int n, Position a, Position b)
+  {
+    if(this->n_step_distance_from(n, a) < this->n_step_distance_from(n, b))
+      return a;
+    else
+      return b;
+  }
+protected:
+  int m_x;
+  int m_y;
+  int m_vx;
+  int m_vy;
+};
+
+class Flag : public Position
 {
 public:
     Flag()
     {
         m_team = "none";
     }
-    
+
     ~Flag()
     {
-        
+
     }
-    
+
     void set_info()
     {
+        int x, y;
         cin >> m_x >> m_y;
         cin.ignore();
         if(m_team == "none")
@@ -30,146 +122,87 @@ public:
                 m_team = "orange";
             else
                 m_team = "purple";
-        
+
         if(m_x ==-1 && m_y == -1)
             m_in_base = false;
         else
             m_in_base = true;
     }
-    
-    int get_x()
-    {
-        return m_x;
-    }
-    
-    int get_y()
-    {
-        return m_y;
-    }
-    
+
     string get_team()
     {
         return m_team;
     }
-    
+
     bool is_in_base()
     {
         return m_in_base;
     }
-    
-private:
-    int m_x;
-    int m_y;
+
+protected:
     string m_team;
-    
+
     bool m_in_base;
 };
 
-class Ovni
+class Ovni : public Position
 {
 public:
     Ovni()
     {
         m_team = "none";
-        m_v_err = 0;
+        m_power = "0";
     }
-    
+
     ~Ovni()
     {
-        
+
     }
-    
+
     void set_info()
     {
         cin >> m_x >> m_y >> m_vx >> m_vy >> m_flag;
+        
+        m_busy = false;
+
         cin.ignore();
     }
-    
+
     void set_team(string team)
     {
         if(m_team == "none")
             m_team = team;
     }
-    
-    int get_x()
-    {
-        return m_x;
-    }
-    
-    int get_y()
-    {
-        return m_y;
-    }
-  
-  	int get_vx()
-    {
-      return m_vx;
-    }
-  
-  	int get_vy()
-    {
-      return m_vy;
-    }
-    
+
     bool has_flag()
     {
         return m_flag;
     }
     
-    void set_target(int x, int y, string power)
+    bool is_busy()
     {
-        if(x >= 0)
-            m_x_target = x;
-        if(y >= 0)
-            m_y_target = y;
-        m_power = power;
+        return m_busy;
     }
-    
+
     void command()
     {
-        cout << m_x_target << ' ' << m_y_target << ' ' << m_power << endl;
+        cout << m_target.get_x() << ' ' << m_target.get_y() << ' ' << m_power << endl;
     }
-    
-    float distance_from(int x, int y)
-    {
-        return sqrt(pow(m_x - x, 2) + pow(m_y - y, 2));
-    }
-    
-    float n_step_distance_from(int n, int x, int y)
-    {
-        return sqrt(pow(m_x + n*m_vx- x, 2) + pow(m_y + n*m_vy - y, 2));
-    }
-    
-    float distance_from(Ovni ovni)
-    {
-        return sqrt(pow(m_x - ovni.get_x(), 2) + pow(m_y - ovni.get_y(), 2));
-    }
-    
-    float n_step_distance_from(int n, Ovni ovni)
-    {
-        return sqrt(pow(m_x + n*m_vx - n*ovni.get_vx() - ovni.get_x(), 2) + pow(m_y + n*m_vy - n*ovni.get_vy() - ovni.get_y(), 2));
-    }
-    
-    float distance_from(Flag flag)
-    {
-        return sqrt(pow(m_x - flag.get_x(), 2) + pow(m_y - flag.get_y(), 2));
-    }
-    
+
+    // Regulate the position of the ovni on its target
     void regulate_speed()
     {
         int speed = 100;
-        
-        float d = this->n_step_distance_from(0,m_x_target, m_y_target);
-        float d_1s = this->n_step_distance_from(1,m_x_target, m_y_target);
-        float d_2s = this->n_step_distance_from(2,m_x_target, m_y_target);
-        
+
+        float d = this->n_step_distance_from(0, m_target);
+        float d_1s = this->n_step_distance_from(1,m_target);
+
         if(d < 100 && m_vx < 10 && m_vy << 10)
             speed = 0;
         else
             if(d_1s < 200)
             {
-                m_x_target -= 100*m_vx;
-                m_y_target -= 100*m_vy;
+                m_target.set_pos(m_target.get_x() - 100*m_vx, m_target.get_y() - 100*m_vy);
                 speed = 100;
             }
             else
@@ -183,205 +216,113 @@ public:
             }
         m_power = to_string(speed);
     }
-    
-    void chase(Ovni enemy)
+
+    void chase(Position enemy)
     {
+        m_busy = true;
         if(m_team == "orange")
         {
-            if(m_x < enemy.get_x())
-                this->set_target(enemy.get_x() + enemy.get_vx(), enemy.get_y() + enemy.get_vy()*2, "BOOST");
-            else
-                this->set_target(enemy.get_x(), enemy.get_y(), "BOOST");
-            
-            // Bounce on border
-            if(m_x < 2000 && m_vx < 0)
-                this->set_target(m_x + 5*m_vx, m_y +5*m_vy, "BOOST");
+            //if(m_x + 500< enemy.get_x())
+              m_target = enemy.get_n_step_pos(2);
+            //else
+            //  m_target = enemy.get_n_step_pos(0);
+
+            // TODO Bounce on border
         }
         else
         {
-            if(m_x > enemy.get_x())
-                this->set_target(enemy.get_x() + enemy.get_vx(), enemy.get_y() + enemy.get_vy()*2, "BOOST");
-            else
-                this->set_target(enemy.get_x(), enemy.get_y(), "BOOST");
-                
-            // Bounce on border
-            if(m_x > 8000 && m_vx > 0)
-                this->set_target(m_x + 5*m_vx, m_y +5*m_vy, "BOOST");
+          //if(m_x  - 500 > enemy.get_x())
+            m_target = enemy.get_n_step_pos(2);
+          //else
+            //m_target = enemy.get_n_step_pos(0);
+
+            // TODO Bounce on border
         }
+
+        m_power = "BOOST";
     }
-    
+
     void return_flag(Ovni enemy_1, Ovni enemy_2)
     {
-        int x_target;
-        int y_target = -1;
+        m_busy = true;
         float theta;
-        
+        Position enemy;
+
         if(m_team == "orange")
         {
+            // If we are going in enemy home, bounce on the wall
             if(m_vx > 0)
             {
-                x_target = m_x + m_vx*10;
-                y_target = m_y + m_vy*10;
+              m_target = this->get_n_step_pos(10);
             }
+            // Else go back to home
             else
-                x_target = 0;
+              m_target.set_x(0);
         }
         else
         {
+              // If we are going in enemy home, bounce on the wall
              if(m_vx < 0)
             {
-                x_target = m_x + m_vx*10;
-                y_target = m_y + m_vy*10;
+                m_target = this->get_n_step_pos(10);
             }
+            // Else go back to home
             else
-                x_target = 10000;
+                m_target.set_x(10000);
         }
-        
-        if(y_target == -1)
-        {
-            float dist_enemy_1, dist_enemy_2, dist_enemy;
-            Ovni enemy;
-            
-            dist_enemy_1 = this->n_step_distance_from(1, enemy_1);
-            dist_enemy_2 = this->n_step_distance_from(1, enemy_2);
-            
-            if(dist_enemy_1 < dist_enemy_2)
-            {
-                enemy = enemy_1;
-                dist_enemy = dist_enemy_1;
-            }
-            else
-            {
-                enemy = enemy_2;
-                dist_enemy = dist_enemy_2;
-            }
-            
-            if(dist_enemy < 500 && m_vy*enemy.get_y() < 0)
-            {
-                y_target = m_y - m_vy;
-            }
-            else
-            {
-                /*theta = atan2(m_vx, m_vy);
-                if(theta > PI/2 && theta < 5*PI/6)
-                    theta = 5*PI/6;
-                if(theta < -PI/2 && theta > -5*PI/6)
-                    theta = -5*PI/6;
-                if(theta < PI/2 && theta > PI/6)
-                    theta = PI/6;
-                if(theta > -PI/2 && theta < -PI/6)
-                    theta = -PI/6;
-                    
-                y_target = m_y - (m_x - x_target)/tan(theta);*/
-                x_target = m_x + m_vx*10;
-                y_target = m_y + m_vy*7;
-            }            
-        }
-        
-        this->set_target(x_target, y_target, "100");
-    }
-    
-    void attack_flag(Flag ally_flag, Flag enemy_flag, Ovni enemy_1, Ovni enemy_2)
-    {
-        // Check if it has to defend
-        if(!ally_flag.is_in_base())
-        {
-            Ovni enemy;
-            // If it is closer to the enemy target the enemy else target the flag
-            if(enemy_1.has_flag())
-                enemy = enemy_1;
-            else
-                enemy = enemy_2;
-                
-            if(this->distance_from(enemy) < this->distance_from(enemy_flag))
-                    this->chase(enemy);
-                else
-                    this->set_target(enemy_flag.get_x(), enemy_flag.get_y(), "BOOST");
-        }
-        // Atack
-        else
-        {
-            if(this->distance_from(enemy_flag) > 6000)
-                this->set_target(enemy_flag.get_x(), enemy_flag.get_y(), "BOOST");
-            else
-                this->set_target(enemy_flag.get_x(), enemy_flag.get_y(), "100");
-        }
-    }
-    
-    void defend_flag(Flag ally_flag, Flag enemy_flag, Ovni enemy_1, Ovni enemy_2)
-    {
-        // Go on the enemy which has the flag
-        Ovni enemy;
-        if(!ally_flag.is_in_base())
-        {
-            if(enemy_1.has_flag())
-                enemy = enemy_1;
-            else
-                enemy = enemy_2;
-            
-            this->chase(enemy);
-        }
-        else
-        // Go between our flag and the enemy
-        {
-            Ovni enemy;
-            int x_target;
-            int y_target;
-            
-            float dist_enemy_1, dist_enemy_2, dist_enemy;
 
-            x_target = ally_flag.get_x();
-            y_target = ally_flag.get_y();
-            
-            this->set_target(x_target, y_target, "100");
-            this->regulate_speed();
-            
-            dist_enemy_1 = enemy_1.distance_from(ally_flag);
-            dist_enemy_2 = enemy_2.distance_from(ally_flag);
-            
-            if(dist_enemy_1 < dist_enemy_2)
-            {
-                enemy = enemy_1;
-                dist_enemy = dist_enemy_1;
-            }
-            else
-            {
-                enemy = enemy_2;
-                dist_enemy = dist_enemy_2;
-            }
-            
-            
-            if(dist_enemy < 2000)
-            {
-                //x_target = enemy.get_x() - enemy.get_vx();
-                //y_target = enemy.get_y() - enemy.get_vy();
-                x_target = m_x + 2*(enemy.get_x() - m_x);
-                y_target = m_y + 2*(enemy.get_y() - m_y);
-                this->set_target(x_target, y_target, "100");
-            }
-        }
+        enemy = this->get_n_step_closest(1, enemy_1, enemy_2);
+
+        // If there is an enemy close, go in the other way
+        if(this->n_step_distance_from(1, enemy) < 500)
+          m_target.set_y(m_y - 4*enemy.get_vy());
+        // Else follow the same way
+        else
+          m_target.set_y(m_y + 4*m_vy);
+
+        m_power = "100";
     }
-    
+
+    void attack_flag(Flag enemy_flag)
+    {
+        m_busy = true;
+      // Go catch the flag
+      m_target = enemy_flag;
+      if(this->n_step_distance_from(1, enemy_flag) > 8000)
+        m_power = "100";
+      else
+        m_power = "BOOST";
+    }
+
+    void defend_flag(Flag ally_flag)
+    {
+        m_busy = true;
+        m_target = ally_flag;
+        this->regulate_speed();
+    }
+
+    void block_flag(Flag ally_flag, Ovni enemy)
+    {
+        m_busy = true;
+        m_target.set_pos(m_x + 5*(enemy.get_x() - m_x), m_y + 5*(enemy.get_y() - m_y));
+        m_power = "100";
+    }
+
     void debug()
     {
         cerr << "x: " << m_x << ", y: " << m_y << ", vx: " << m_vx << ", vy: " << m_vy << endl;
     }
-    
-private:
- int m_x;
- int m_y;
- int m_vx;
- int m_vy;
+
+protected:
  bool m_flag;
- 
- float m_v_err;
- 
+
+ bool m_busy;
+
  string m_team;
- 
- int m_x_target;
- int m_y_target;
+
+ Position m_target;
  string m_power;
-    
+
 };
 
 /**
@@ -390,71 +331,90 @@ private:
  **/
 int main()
 {
-    Ovni ally_1, ally_2, enemy_1, enemy_2;
-    
+    Ovni ally_1, ally_2, enemy_1, enemy_2, enemy;
+    Ovni* ally;
+
     Flag ally_flag, enemy_flag;
-    
+
 // game loop
     while (1) {
 // Get the data
-        
+
         // Ally flag
         ally_flag.set_info();
-        
+
         // Enemy flag
         enemy_flag.set_info();
-        
+
         // Ally 1
         ally_1.set_info();
         ally_1.set_team(ally_flag.get_team());
-        
+
         // Ally 2
         ally_2.set_info();
         ally_2.set_team(ally_flag.get_team());
-        
+
         // Enemy 1
         enemy_1.set_info();
         enemy_1.set_team(enemy_flag.get_team());
-        
+
         // Enemy 2
         enemy_2.set_info();
         enemy_2.set_team(enemy_flag.get_team());
-        
+
 // Process game
 
         if(enemy_flag.is_in_base())
         {
-            if(ally_1.distance_from(enemy_flag) < ally_2.distance_from(enemy_flag))
-            {
-                ally_1.attack_flag(ally_flag, enemy_flag, enemy_1, enemy_2);
-                ally_2.defend_flag(ally_flag, enemy_flag, enemy_1, enemy_2);
-            }
+            ally = (ally_1.n_step_distance_from(0, enemy_flag) < ally_2.n_step_distance_from(0, enemy_flag)) ? &ally_1 : &ally_2;
+            ally->attack_flag(enemy_flag);
+        }
+
+        if(ally_flag.is_in_base())
+        {
+            enemy = (ally_flag.n_step_distance_from(0, enemy_1) < ally_flag.n_step_distance_from(0, enemy_2)) ? enemy_1 : enemy_2;
+            ally = (ally_1.n_step_distance_from(0, ally_flag) < ally_2.n_step_distance_from(0, ally_flag)) ? &ally_1 : &ally_2;
+
+            if(enemy.n_step_distance_from(1, ally_flag) > 1000)
+                ally->defend_flag(ally_flag);
             else
-            {
-                ally_1.defend_flag(ally_flag, enemy_flag, enemy_1, enemy_2);
-                ally_2.attack_flag(ally_flag, enemy_flag, enemy_1, enemy_2);
-            }
+                ally->block_flag(ally_flag, enemy);
         }
         else
         {
-            if(ally_1.has_flag())
-            {
+            enemy = (enemy_1.has_flag()) ? enemy_1 : enemy_2;
+            ally = (ally_1.n_step_distance_from(0, enemy) < ally_2.n_step_distance_from(0, enemy)) ? &ally_1 : &ally_2;
+
+            ally->chase(enemy);
+        }
+        
+        if(ally_1.has_flag())
                 ally_1.return_flag(enemy_1, enemy_2);
-                ally_2.defend_flag(ally_flag, enemy_flag, enemy_1, enemy_2);
-            }
+        if(ally_2.has_flag())
+                ally_2.return_flag(enemy_1, enemy_2);
+        
+        if(!ally_1.is_busy())
+            if(ally_flag.is_in_base())
+                ally_1.defend_flag(ally_flag);
             else
             {
-                ally_1.defend_flag(ally_flag, enemy_flag, enemy_1, enemy_2);
-                ally_2.return_flag(enemy_1, enemy_2);
+                enemy = (enemy_1.has_flag()) ? enemy_1 : enemy_2;
+                ally_1.chase(enemy);
             }
-        }
-                
-        ally_1.debug();
+            
+        if(!ally_2.is_busy())
+            if(ally_flag.is_in_base())
+                ally_2.defend_flag(ally_flag);
+            else
+            {
+                enemy = (enemy_1.has_flag()) ? enemy_1 : enemy_2;
+                ally_2.chase(enemy);
+            }
         
 // Ouptut command
         // Command ovni 1
         ally_1.command();
-        
+
         // Command ovni 2
         ally_2.command();
     }
